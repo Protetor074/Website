a0c831391a140cdd098f85d1fb1e50b9d2d97c41
Protetor074 <101059740+Protetor074@users.noreply.github.com>
2023-07-02 13:23:41 +0200

    Upload

---
+++ conf/anim.js
@@ -0,0 +1,23 @@
+function zegarek()
+{
+	
+	var dzisiaj = new Date();
+	var dzien = dzisiaj.getDate();
+	if(dzien<10)dzien = "0"+dzien;
+	var miesiac = dzisiaj.getMonth()+1;
+	if(miesiac<10)miesiac = "0"+miesiac;
+	var rok = dzisiaj.getFullYear();
+
+	var godzina = dzisiaj.getHours();
+	if(godzina<10)godzina = "0"+godzina;
+	var minuta = dzisiaj.getMinutes();
+	if(minuta<10)minuta = "0"+minuta;
+	var sekunda = dzisiaj.getSeconds();
+	if(sekunda<10)sekunda = "0"+sekunda;
+
+	document.getElementById("zegar").innerHTML = godzina+":"+minuta+":"+sekunda;
+	document.getElementById("data0").innerHTML = "Data:"+dzien+"/"+miesiac+"/"+rok;
+	
+	setTimeout("zegarek()",1000);
+}
+
